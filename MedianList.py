666f3705a95f91f2a0293a2bf32726862b9df1c8
Ayush S. Nair <134037589+Ayush201738982361@users.noreply.github.com>
2023-09-10 23:09:57 +0530

    Add files via upload

---
+++ MedianList.py
@@ -0,0 +1,25 @@
+import statistics 
+a = []
+size = int(input("Enter The Size Of The List:"))
+for i in range(size):
+    val = int(input("Enter The Value:"))
+    a.append(val)
+print("Unordered List:", a)
+a.sort()
+print("Ordered List:", a)
+
+def median(a):
+        if (len(a)%2 != 0):
+            middle=a[len(a//2)]
+            print("Median Of This List Is:",middle)
+
+        else:
+            if (len(a)%2 == 0):
+                first_middle=a[len(a//2)]
+                second_middle=a[len(a//2)]
+                result=first_middle/ second_middle
+                print
+            median(a)
+            
+                    
+        
